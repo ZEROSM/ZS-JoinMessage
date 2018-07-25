14ae9a47b8aeb2d84b4f1034700096abd7d27ce9
MineDefenser <mcpesunho@naver.com>
2018-07-25 13:07:30 +0900

    필요없는 이벤트 소스 제거

--- src/NewZerosm/JoinMessage.php
+++ src/NewZerosm/JoinMessage.php
@@ -3,44 +3,11 @@
 namespace ZEROSM;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\math\Vector3;
-use pocketmine\item\Item;
-use pocketmine\level\Level;
-use pocketmine\event\TranslationContainer;
-use pocketmine\network\protocol\TransferPacket;
-use pocketmine\level\Position;
-use pocketmine\entity\Effect;
-use pocketmine\entity\InstantEffect;
-use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerChangeSkinEvent;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\inventory\InventoryPickupItemEvent;
-use pocketmine\event\inventory\CraftItemEvent;
-
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
-use pocketmine\network\mcpe\protocol\ServerSettingsRequestPacket;
-use pocketmine\network\mcpe\protocol\ServerSettingsResponsePacket;
-
-use pocketmine\event\player\cheat\PlayerIllegalMoveEvent;
-
-use jojoe77777\FormAPI;
-use onebone\economyapi\EconomyAPI;
-use ZEROSM\Tag;
 
 class JoinMessage extends PluginBase implements Listener{
 
